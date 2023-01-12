bf25f7ce9375e9ed76b30641db15f2f726eb9056
monarkmehta <monark@stampmyvisa.com>
2023-01-12 15:43:38 +0530

    First release of Hello World!

---
+++ backend/data.js
@@ -0,0 +1,76 @@
+const data={
+    products: [
+        {
+            name:"Trolly one",
+            slug:"trolly-one",
+            category:"Trolly",
+            image:"/images/1.jpg",
+            price: 2200,
+            countInStock: 10,
+            brand:"amazon",
+            rating: 4.5,
+            numReviews: 10,
+            description :"high quality bag",
+
+        },
+
+        {
+            name:"Trolly Safari",
+            slug:"trolly-safari",
+            category:"Trolly",
+            image:"/images/2.jpg",
+            price: 3200,
+            countInStock: 8,
+            brand:"safari",
+            rating: 4.4,
+            numReviews: 10,
+            description :"high quality bag",
+
+        },
+
+        {
+            name:"Trolly American",
+            slug:"trolly-american",
+            category:"Trolly",
+            image:"/images/3.jpg",
+            price: 3600,
+            countInStock: 10,
+            brand:"American Tourister",
+            rating: 4.7,
+            numReviews: 9,
+            description :"Premium quality bag",
+
+        },
+
+        {
+            name:"Bag Pack One",
+            slug:"bag-pack-one",
+            category:"Bag Pack",
+            image:"/images/b1.jpg",
+            price: 1200,
+            countInStock: 10,
+            brand:"American Tourister",
+            rating: 4.3,
+            numReviews: 10,
+            description :"high quality bag pack",
+
+        },
+
+        {
+            name:"purse one",
+            slug:"purse-one",
+            category:"Ladies Purse",
+            image:"/images/p1.jpg",
+            price: 800,
+            countInStock: 10,
+            brand:"amazon",
+            rating: 4.1,
+            numReviews: 10,
+            description :"high quality Ladies purse",
+
+        },
+    ],
+};
+
+export default data;
+
